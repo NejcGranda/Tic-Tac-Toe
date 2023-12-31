d1bcaea42661a6ad314dc774007aabb114257213
NejcGranda <nejc.granda10136@gmail.com>
2023-12-31 13:43:57 +0100

    functions for squares

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+import numpy as np
 pygame.init()
 
 def draw_lines():
@@ -14,15 +14,42 @@ def draw_lines():
     # vertical line 2
     pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
 
+# označi kvadrat
+def mark_square(row, col, player):
+    board[row] [col] = player
+
+# ali je kvadrat prost
+def avaible_square(row, col):
+    if board [row][col] == 0:
+        return True
+    else:
+        return False
+    
+# ali so vsa mesta zapolnjena
+def is_full():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board [row] [col] == 0:
+                return False
+            
+    return True
+
+
 WIDTH = 600
 HEIGHT = 600
 LINE_WIDTH = 7
+BOARD_ROWS = 3
+BOARD_COLS = 3
 LINE_COLOR = (0, 0, 0)
 BG_COLOR = (0, 255, 255)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))  
 title = pygame.display.set_caption("Tic Tac Toe") 
 screen.fill(BG_COLOR)
+
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+
+
 
 draw_lines()
 
